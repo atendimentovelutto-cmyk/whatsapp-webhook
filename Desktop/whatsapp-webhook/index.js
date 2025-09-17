cea182dbabfbe7a4aaa45235e7e15b2997de1df5
Anne Velutto <atendimento.velutto@gmail.com>
2025-09-17 15:45:07 -0300

    primeira versão webhook

---
+++ Desktop/whatsapp-webhook/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+// Rota para validar webhook com o Meta
+app.get("/webhook", (req, res) => {
+  const verify_token = "velutto123"; // troque por um token que você inventar
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token && mode === "subscribe" && token === verify_token) {
+    res.status(200).send(challenge); // responde o hub.challenge
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// Rota para receber mensagens
+app.post("/webhook", (req, res) => {
+  console.log("Mensagem recebida:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200); // confirma recebimento para o WhatsApp
+});
+
+
+// Inicia servidor na porta definida pelo Render ou 3000 localmente
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
